c305a3b42925a79747bd72612fce5285aa3a6bee
yukimatsuda0321 <yu-matsuda@enecom.co.jp>
2025-07-17 09:16:20 +0900

    カードコンポーネントにリンクを追加

--- src/components/post/PostCard.tsx
+++ src/components/post/PostCard.tsx
@@ -12,27 +12,30 @@ import {
     CardTitle,
 } from "@/components/ui/card"
 import { PostCardProps } from "@/types/post"
+import Link from "next/link"
 
 export default function PostCard({ post }: PostCardProps) {
     console.log(post.createdAt)
     return (
         <Card className="shadow-lg rounded-lg transition-transform hover:scale-[1.01] hover:shadow-xl duration-200">
-            {post.topImage && (
-                <div className="relative w-full h-48">
-                    <img src="image.png" alt="画像が見つかりません" width="250px" className="rounded-t-md object-cover" />
-                </div>
-            )}
-            <CardHeader>
-                <CardTitle>
-                    <time>{format(new Date(post.createdAt), "yyyy/M/d HH:mm:ss", { locale: ja })}</time>
-                </CardTitle>
-            </CardHeader>
-            <CardContent>
-                <p className="font-bold">Dr. {post.author.name}</p>
-                <p className="mt-2 text-sm text-gray-600 mb-2 line-clamp-2">
-                    {post.content}
-                </p>
-            </CardContent>
+            <Link href="video">
+                {post.topImage && (
+                    <div className="relative w-full h-48">
+                        <img src="image.png" alt="画像が見つかりません" width="225px" className="ml-12 rounded-t-md object-cover" />
+                    </div>
+                )}
+                <CardHeader>
+                    <CardTitle>
+                        <time>{format(new Date(post.createdAt), "yyyy/M/d HH:mm:ss", { locale: ja })}</time>
+                    </CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <p className="font-bold">Dr. {post.author.name}</p>
+                    <p className="mt-2 text-sm text-gray-600 mb-2 line-clamp-2">
+                        {post.content}
+                    </p>
+                </CardContent>
+            </Link>
         </Card>
 
     )
